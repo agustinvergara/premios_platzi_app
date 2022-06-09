4bfde943165bc9dc07831d9c5a66ccfc919c8a36
chuxi <agustin.vergara.ruiz@gmail.com>
2022-06-09 10:00:59 -0500

    starting to practice with templates

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #ex: ''/ we are in the index
     path('', views.index, name='index'),
+    #ex: polls/5/ we are seeing the details of the question #5 (question_id)
+    path('<int:question_id>/', views.detail, name='detail'),
+    #ex: /polls/5/results/ we are seeing the results of the question #5 (question_id)
+    path('<int:question_id>/results/', views.results, name='results'),
+    #ex: polls/5/vote/ we are voting for the question #5 (question_id)
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse('hello, world. You are at the polls index')
+    latest_question_list = 
+    return HttpResponse('estas en la pagina principal')
 
-def chucha(request):
-    return HttpResponse('chucha xd')
-# Create your views here.
+def detail(request, question_id):
+    return HttpResponse(f'estas viendo la pregunta no. {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'estas viendo los resultados de la pregunta no. {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'estas votando a la pregunta no. {question_id}')
